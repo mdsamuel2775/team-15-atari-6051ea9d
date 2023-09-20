6f0ec378a18de0ba338ca64327e7a7d3f98324cd
mdsamuel2775 <59888690+mdsamuel2775@users.noreply.github.com>
2023-09-20 15:33:14 -0400

    Update MoveLibrary.py

--- tests/robot/MoveLibrary.py
+++ tests/robot/MoveLibrary.py
@@ -16,3 +16,11 @@ def initialize_character_yposition_with(self, y_position):
 def move_in_direction(self, direction):
   self.controller.set_character_position((self.start_x, self.start_y))
   self.controller.move(Direction[direction])
+
+def character_xposition_should_be(self, expected):
+  end_x = self.controller.status.current_position[0]
+  assert end_x == expected, f"Expected: {expected}, Actual: {end_x}"
+
+def character_yposition_should_be(self, expected):
+  end_y = self.controller.status.current_position[1]
+  assert end_y == expected, f"Expected: {expected}, Actual: {end_y}"
